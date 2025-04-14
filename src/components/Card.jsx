7c7bfd8bc196d923eee2430a328dce42f60b6be8
shishirjinu <shishirpradhan188@gmail.com>
2025-04-14 20:36:06 +0530

    card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Card = ({ children }) => {
     return (
+        <div
+            className="flex items-center justify-center min-h-screen bg-cover bg-center dark:bg-gray-800 dark:border-gray-500">
 
-        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-            {children}
+            <div className="w-lvh max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-900 dark:border-gray-700">
+                {children}
+            </div>
         </div>
 
     )
